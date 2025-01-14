dd989b8fee44b3c1a5a2a9adcde85d0e166127e1
Sonu Kumar <sonukumar7004923959@gmail.com>
2025-01-14 21:42:36 +0530

    adding coin functions

--- script.js
+++ script.js
@@ -10,16 +10,82 @@ const box9 = document.getElementById("box9");
 let chance = document.getElementById("chance");
 let player = "X";
 const boxes = document.querySelectorAll(".box");
+
 // Add click event to all boxes
 boxes.forEach((box) => {
   box.addEventListener("click", handleBoxClick);
 });
+
 // Function to remove click event listeners
 function removeClickEventListeners() {
   boxes.forEach((box) => {
     box.removeEventListener("click", handleBoxClick);
   });
 }
+
+const coin = document.querySelector(".coin");
+const tossButton = document.getElementById("toss-button");
+const front = document.querySelector(".front");
+const back = document.querySelector(".back");
+
+tossButton.addEventListener("click", () => {
+  coin.classList.add("flip");
+  setTimeout(() => {
+    coin.classList.remove("flip");
+    const result = Math.random();
+    let outcome;
+
+    if (result < 0.5) {
+      outcome = "Heads";
+      front.style.transform = "rotateY(0deg)";
+      back.style.transform = "rotateY(180deg)";
+      player = "X";
+      chance.textContent = "Player X, Play Your First Move";
+      playerX.classList.add("active");
+      playerO.classList.remove("active");
+      setTimeout(function () {
+        displayResult(outcome);
+      }, 1500);
+    } else {
+      outcome = "Tails";
+      front.style.transform = "rotateY(180deg)";
+      back.style.transform = "rotateY(0deg)";
+      player = "O";
+      chance.textContent = "Player O, Play Your First Move";
+      playerO.classList.add("active");
+      playerX.classList.remove("active");
+      setTimeout(function () {
+        displayResult(outcome);
+      }, 1500);
+    }
+  }, 3000);
+});
+
+const tossScetion = document.querySelector(".toss-section");
+function displayTossCoin() {
+  const gameContainer = document.querySelector(".container");
+  const tossScetion = document.querySelector(".toss-section");
+  tossScetion.classList.add("displayCoin");
+  gameContainer.classList.add("hideContainer");
+  document.getElementById("restart").style.display = "none";
+}
+
+document.getElementById("tossACoin").addEventListener("click", displayTossCoin);
+function displayResult(outcome) {
+  tossScetion.classList.remove("displayCoin");
+  document.querySelector(".result").textContent = outcome;
+  document.querySelector(".result").classList.add("show");
+  hideResult();
+}
+
+function hideResult() {
+  setTimeout(() => {
+    document.querySelector(".result").classList.remove("show");
+    document.querySelector(".container").classList.remove("hideContainer");
+    document.getElementById("restart").style.display = "";
+  }, 4000);
+}
+
 // Handle click function on boxes
 function handleBoxClick() {
   if (this.textContent === "") {
@@ -163,8 +229,7 @@ function resetGame() {
     box.textContent = "";
     box.removeAttribute("data-content");
     box.style.backgroundColor = "red";
-    resetHighlightPlayer()
-
+    resetHighlightPlayer();
   });
 
   player = "X";
@@ -189,20 +254,21 @@ function highlightPlayer() {
 }
 
 function resetHighlightPlayer() {
-  chance.textContent = "Start game or select player";
-  if (playerX.classList.contains("active")) {
-    playerX.classList.add("active");
-    playerO.classList.remove("active");
-  } else {
-    playerO.classList.remove("active");
-    playerX.classList.add("active");
-  }
+  chance.innerHTML =
+    "Start game or select player or <span id='tossACoin'>Toss a coin</span>";
+  document
+    .getElementById("tossACoin")
+    .addEventListener("click", displayTossCoin);
+
+  playerO.classList.remove("active");
+
+  playerX.classList.remove("active");
 }
 
 let scoreOfx = 0;
 let scoreOfo = 0;
-const scorex = document.getElementById('scorex');
-const scoreo = document.getElementById('scoreo');
+const scorex = document.getElementById("scorex");
+const scoreo = document.getElementById("scoreo");
 function updateScore() {
   const winningPlayer = winnerPlayer();
   if (winningPlayer && winningPlayer.player === "X") {
@@ -227,35 +293,9 @@ document.getElementById("restart").addEventListener("click", restart);
 function restart() {
   resetScores();
   resetGame();
-  chance.textContent = "Start game or select player";
+  chance.innerHTML =
+    "Start game or select player or <span id='tossACoin'>Toss a coin</span>";
+  document
+    .getElementById("tossACoin")
+    .addEventListener("click", displayTossCoin);
 }
-
-
-
-const coin = document.querySelector('.coin');
-const tossButton = document.getElementById('toss-button');
-const front = document.querySelector('.front');
-const back = document.querySelector('.back');
-
-tossButton.addEventListener('click', () => {
-    coin.classList.add('flip');
-    setTimeout(() => {
-        coin.classList.remove('flip');
-        const result = Math.random();
-        let outcome;
-
-        if (result < 0.5) {
-            outcome = 'Heads';
-            front.style.transform = 'rotateY(0deg)';
-            back.style.transform = 'rotateY(180deg)';
-             
-        } else {
-            outcome = 'Tails';
-            front.style.transform = 'rotateY(180deg)';
-            back.style.transform = 'rotateY(0deg)';
-           
-        }
-
-       
-    }, 3000);
-});
